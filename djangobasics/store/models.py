b560afdb176cb37f7e41757cd5150bd127d77389
remygist <remy.gistelinck@student.ehb.be>
2024-10-15 15:37:58 +0200

    add a default timedate to articles publish date

--- djangobasics/store/models.py
+++ djangobasics/store/models.py
@@ -1,4 +1,12 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Articles(models.Model):
     article_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published", default=timezone.now())
+    def __str__(self):
+        return self.article_name
+    def date_published(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
