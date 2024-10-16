23d22e35d182d6a96544948cff7eb9b993f880b5
remygist <remy.gistelinck@student.ehb.be>
2024-10-16 10:07:25 +0200

    made an article overview and detail page

--- djangobasics/store/urls.py
+++ djangobasics/store/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "store"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:article_id>/", views.detail, name="detail")
 ]
--- djangobasics/store/views.py
+++ djangobasics/store/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Article
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the store index.")
+    latest_articles_list = Article.objects.order_by("-pub_date")
+    context = {
+        "latest_articles_list": latest_articles_list,
+    }
+    return render(request, "store/index.html", context)
+
+def detail(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    return render(request, "store/detail.html", {"article": article})
+
+def article_create(request):
+    return HttpResponse("This is the create article page!.")
