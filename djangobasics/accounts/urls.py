acce6ab43aa9b8381d095adea7dfff4738eecb35
remygist <remy.gistelinck@student.ehb.be>
2024-10-17 10:18:21 +0200

    made the password change page

--- djangobasics/accounts/urls.py
+++ djangobasics/accounts/urls.py
@@ -5,5 +5,4 @@ from .views import SignUpView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
-    path("accounts/login/", auth_views.LoginView.as_view, name="login")
 ]
