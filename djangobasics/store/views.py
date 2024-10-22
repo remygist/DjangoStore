2eddfb9ae78da4658ca7ee1591981276baf99415
remygist <remy.gistelinck@student.ehb.be>
2024-10-22 10:09:49 +0200

    prevent adding duplicate articles

--- djangobasics/store/views.py
+++ djangobasics/store/views.py
@@ -13,23 +13,30 @@ def index(request):
     }
     return render(request, "store/index.html", context)
 
+
+
 def detail(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
     return render(request, "store/detail.html", {"article": article})
+
+
 
 @csrf_protect
 @login_required
 def article_create(request):
     if request.method == "POST":
         article_name = request.POST.get("article_name")
-
-        if article_name:
+        if Article.objects.filter(article_name=article_name).exists():
+            return render(request, "store/create.html", {"error":"This article already exists."})
+        elif article_name:
             new_article = Article(article_name=article_name)
             new_article.save()
             return redirect("store:index")
         else:
             return HttpResponse("Article name cannot be empty", status=400)
     return render(request, "store/create.html")
+
+
 
 def article_delete(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
