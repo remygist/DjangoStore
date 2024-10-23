6c138bfcd9d0bc97262ad837c0081fb8b6d324f9
remygist <remy.gistelinck@student.ehb.be>
2024-10-23 15:58:47 +0200

    restyled the create page

--- djangobasics/store/models.py
+++ djangobasics/store/models.py
@@ -5,6 +5,12 @@ from django.utils import timezone
 
 class Article(models.Model):
     article_name = models.CharField(max_length=200)
+<<<<<<< Updated upstream
+=======
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='store/files/product_images', null=True)
+>>>>>>> Stashed changes
     pub_date = models.DateTimeField("date published", default=timezone.now())
     def __str__(self):
         return self.article_name
