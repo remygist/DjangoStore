ee051c78cb49977420d7066857061e8326e4083e
remygist <remy.gistelinck@student.ehb.be>
2024-10-23 11:34:53 +0200

    merged the index and the shopping cart page

--- djangobasics/store/urls.py
+++ djangobasics/store/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     path("articles/<int:article_id>/", views.detail, name="article_detail"),
     path("articles/create/", views.article_create, name="article_create"),
     path("articles/<int:article_id>/delete/", views.article_delete, name="article_delete"),
-    path("cart/", views.view_cart, name="view_cart"),
-    path("cart/add_to_cart/<int:article_id>/", views.add_to_cart, name='add_to_cart'),
-    path("cart/remove/<int:item_id>", views.remove_from_cart, name="remove_from_cart")
-   
+    path('add-to-cart/<int:article_id>/', views.add_to_cart, name='add_to_cart'),
+    path("cart/remove/<int:item_id>", views.remove_from_cart, name="remove_from_cart"),
 ]
--- djangobasics/store/views.py
+++ djangobasics/store/views.py
@@ -6,20 +6,9 @@ from django.contrib.auth.decorators import login_required
 from .models import Article,CartItem
 
 
-def index(request):
-    latest_articles_list = Article.objects.order_by("-pub_date")
-    context = {
-        "latest_articles_list": latest_articles_list,
-    }
-    return render(request, "store/index.html", context)
-
-
-
 def detail(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
     return render(request, "store/detail.html", {"article": article})
-
-
 
 @csrf_protect
 @login_required
@@ -36,7 +25,6 @@ def article_create(request):
             return HttpResponse("Article name cannot be empty", status=400)
     return render(request, "store/create.html")
 
-
 @login_required
 def article_delete(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
@@ -45,17 +33,25 @@ def article_delete(request, article_id):
         return redirect("store:index")
     return render(request, 'store/article_confirm_delete.html', {'article': article})
 
-def view_cart(request):
-    cart_items = CartItem.objects.filter(user = request.user)
+def index(request, article_id=None):  
+    latest_articles_list = Article.objects.order_by("-pub_date")
+    cart_items = CartItem.objects.filter(user=request.user)
     total_price = sum(item.article.price * item.quantity for item in cart_items)
-    return render(request, 'store/cart.html', {'cart_items': cart_items, 'total_price': total_price})
+
+    context = {
+        "latest_articles_list": latest_articles_list,
+        "cart_items": cart_items,
+        "total_price": total_price
+    }
+
+    return render(request, "store/index.html", context)
 
 def add_to_cart(request, article_id):
-    article = Article.objects.get(id = article_id)
-    cart_item, created = CartItem.objects.get_or_create(article = article, user=request.user)
+    article = get_object_or_404(Article, id=article_id)
+    cart_item, created = CartItem.objects.get_or_create(article=article, user=request.user)
     cart_item.quantity += 1
     cart_item.save()
-    return redirect('store:view_cart')
+    return redirect('store:index')
 
 def remove_from_cart(request, item_id):
     cart_item = CartItem.objects.get(id=item_id)
